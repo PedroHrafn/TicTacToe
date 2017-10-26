2e6be9923ef1adc8075b28d230289bf72a0a77a4
Karisv <karis15@ru.is>
2017-10-26 18:18:55 +0000

    test, check if o is same in checkIfSame()

--- src/test/java/is/ru/tictactoeProject/tictactoeTest.java
+++ src/test/java/is/ru/tictactoeProject/tictactoeTest.java
@@ -123,11 +123,17 @@ public class tictactoeTest {
 		tictactoe game = new tictactoe();
 		assertEquals(false, game.checkIfSame('o', 'x', 'x'));
 	}
-	}
+	
 	@Test
 	public void testCheckIfSamePlayerX() {
 		tictactoe game = new tictactoe();
 		assertEquals(true, game.checkIfSame('x', 'x', 'x'));
 
 	}
-} 
+	@Test
+	public void testCheckIfSamePlayerO() {
+		tictactoe game = new tictactoe();
+		assertEquals(true, game.checkIfSame('o', 'o', 'o'));
+
+	}
+}  
