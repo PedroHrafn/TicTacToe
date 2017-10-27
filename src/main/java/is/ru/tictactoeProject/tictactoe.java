ca71a7b0bb18b2f6f31334a3f712795d967af5e0
johannegi <johannegi16@ru.is>
2017-10-27 12:23:18 +0000

    printboard

--- src/main/java/is/ru/tictactoeProject/tictactoe.java
+++ src/main/java/is/ru/tictactoeProject/tictactoe.java
@@ -25,6 +25,19 @@ public class tictactoe {
     public char[][] getBoard() {
     	return board;
     }
+
+    public void printBoard() {
+        System.out.println("-------------");
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
 	
 	 public boolean getGameOver() {
     	return gameOver;
